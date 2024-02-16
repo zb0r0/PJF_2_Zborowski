e32175bc81cd118b39f37145da498abc37b63580
zb0r0 <krystin52@gmail.com>
2024-02-16 10:05:08 +0100

    lista

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QMainWindow, QWidget, QApplication, QVBoxLayout, QLineEdit, QPushButton, QListWidget
+from PySide6.QtWidgets import QMainWindow, QWidget, QApplication, QVBoxLayout, QLineEdit, QPushButton, QListWidget, QMessageBox
 
 
 
@@ -20,21 +20,33 @@ class MyWidget(QMainWindow):
         self.dodaj_zadanie_przycisk = QPushButton("Dodaj zadanie")
         self.dodaj_zadanie_przycisk.clicked.connect(self.dodaj_zadanie)
 
-        self.dodawanie_zadania = QListWidget()
+        self.lista_zadan = QListWidget()
 
         self.usun_zadanie_przycisk = QPushButton('Usun zadanie')
         self.usun_zadanie_przycisk.clicked.connect(self.usun_zadanie)
 
         self.layout.addWidget(self.dodawanie_zadania)
         self.layout.addWidget(self.dodaj_zadanie_przycisk)
-        self.layout.addWidget(self.zadania)
+        self.layout.addWidget(self.lista_zadan)
         self.layout.addWidget(self.usun_zadanie_przycisk)
 
     def dodaj_zadanie(self):
-        pass
+        zadanie_nazwa = self.dodawanie_zadania.text()
+        if zadanie_nazwa:
+            self.zadania.append(zadanie_nazwa)
+            self.lista_zadan.addItem(zadanie_nazwa)
+            self.dodawanie_zadania.clear()
+        else:
+            QMessageBox.warning(self, "Błąd", "Nazwa zadania nie może być pusta")
 
     def usun_zadanie(self):
-        pass
+        zaznaczony_wiersz = self.lista_zadan.currentRow()
+        if zaznaczony_wiersz != -1:
+            del self.zadania[zaznaczony_wiersz]
+            self.lista_zadan.takeItem(zaznaczony_wiersz)
+        else:
+            QMessageBox.warning(self, "Błąd", "Wybierz zadanie do usuniecia")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
