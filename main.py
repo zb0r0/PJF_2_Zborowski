5854921e9051aec1e5025b69dc5adabd8bca9fa4
zb0r0 <krystin52@gmail.com>
2024-02-17 19:20:32 +0100

    punkty pokera

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import sqlite3
-from PySide6.QtWidgets import QMainWindow, QWidget, QApplication, QVBoxLayout, QLineEdit, QPushButton, QListWidget, QMessageBox, QHBoxLayout, QLabel, QComboBox
 
+from PySide6 import QtGui, QtCore
+from PySide6.QtWidgets import QMainWindow, QWidget, QApplication, QVBoxLayout, QLineEdit, QPushButton, QListWidget, QMessageBox, QHBoxLayout, QLabel, QComboBox, QListWidgetItem
 
 class MyWidget(QMainWindow):
     def __init__(self):
@@ -11,7 +12,7 @@ class MyWidget(QMainWindow):
         self.conn = sqlite3.connect("tasks.db")
         self.cur = self.conn.cursor()
 
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT, project TEXT)''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT, project TEXT, status TEXT)''')
         self.conn.commit()
 
         self.central_widget = QWidget()
@@ -37,8 +38,16 @@ class MyWidget(QMainWindow):
 
         self.lista_zadan = QListWidget()
 
-        self.usun_zadanie_przycisk = QPushButton('Usun zadanie')
+        self.usun_zadanie_przycisk = QPushButton('Usuń zadanie')
         self.usun_zadanie_przycisk.clicked.connect(self.usun_zadanie)
+
+        self.status_label = QLabel("Status:")
+        self.status_combobox = QComboBox()
+        self.status_combobox.addItems(["Do zrobienia", "W trakcie", "Wykonane"])
+        self.status_combobox.currentIndexChanged.connect(self.zmien_status)
+        self.status_layout = QHBoxLayout()
+        self.status_layout.addWidget(self.status_label)
+        self.status_layout.addWidget(self.status_combobox)
 
         self.layout.addWidget(self.dodawanie_projektu)
         self.layout.addWidget(self.dodaj_projekt_przycisk)
@@ -47,6 +56,19 @@ class MyWidget(QMainWindow):
         self.layout.addWidget(self.dodaj_zadanie_przycisk)
         self.layout.addWidget(self.lista_zadan)
         self.layout.addWidget(self.usun_zadanie_przycisk)
+        self.layout.addLayout(self.status_layout)
+
+        self.poker_planning_label = QLabel("Poker Planning:")
+        self.poker_planning_edit = QLineEdit()
+        self.poker_planning_layout = QHBoxLayout()
+        self.poker_planning_layout.addWidget(self.poker_planning_label)
+        self.poker_planning_layout.addWidget(self.poker_planning_edit)
+
+        self.poker_planning_button = QPushButton("Start Poker Planning")
+        self.poker_planning_button.clicked.connect(self.start_poker_planning)
+
+        self.layout.addLayout(self.poker_planning_layout)
+        self.layout.addWidget(self.poker_planning_button)
 
         self.wczytaj_projekty()
 
@@ -62,8 +84,9 @@ class MyWidget(QMainWindow):
         zadanie_nazwa = self.dodawanie_zadania.text()
         projekt = self.projekt_combobox.currentText()
         if zadanie_nazwa:
-            zadanie_do_dodania = f"{zadanie_nazwa} - {projekt}"  # Dodanie separatora między zadaniem a projektem
-            self.cur.execute('''INSERT INTO tasks (name, project) VALUES (?, ?)''', (zadanie_do_dodania, projekt))
+            zadanie_do_dodania = f"{zadanie_nazwa} - {projekt}"
+            self.cur.execute('''INSERT INTO tasks (name, project, status) VALUES (?, ?, ?)''',
+                             (zadanie_do_dodania, projekt, "Do zrobienia"))
             self.conn.commit()
             self.wczytaj_zadania()
             self.dodawanie_zadania.clear()
@@ -75,32 +98,79 @@ class MyWidget(QMainWindow):
         if zaznaczone_zadanie is not None:
             zadanie_text = zaznaczone_zadanie.text()
             zadanie_parts = zadanie_text.split(" - ")
-            if len(zadanie_parts) == 2:
-                zadanie_nazwa, projekt = zadanie_parts
-                self.cur.execute('''DELETE FROM tasks WHERE name = ? AND project = ?''', (zadanie_text, projekt))
-                print(zadanie_nazwa, projekt)
-            elif len(zadanie_parts) == 1:
-                zadanie_nazwa = zadanie_parts[0]
-                self.cur.execute('''DELETE FROM tasks WHERE name = ? AND project IS NULL''', (zadanie_nazwa,))
-
+            name = f"{zadanie_parts[0]} - {zadanie_parts[1]}"
+            projekt = zadanie_parts[1]
+            if len(zadanie_parts) == 3:
+                self.cur.execute('''DELETE FROM tasks WHERE name = ? AND project = ?''', (name, projekt))
+            elif len(zadanie_parts) == 2:
+                self.cur.execute('''DELETE FROM tasks WHERE name = ? AND project IS NULL''', (name,))
             self.conn.commit()
             self.wczytaj_zadania()
         else:
             QMessageBox.warning(self, "Błąd", "Wybierz zadanie do usunięcia")
 
+    def zmien_status(self):
+        zaznaczone_zadanie = self.lista_zadan.currentItem()
+        if zaznaczone_zadanie is not None:
+            nowy_status = self.status_combobox.currentText()
+            zadanie_text = zaznaczone_zadanie.text()
+            zadanie_parts = zadanie_text.split(" - ")
+            name = f"{zadanie_parts[0]} - {zadanie_parts[1]}"
+            projekt = zadanie_parts[1]
+            if len(zadanie_parts) == 3:
+                self.cur.execute('''UPDATE tasks SET status = ? WHERE name = ? AND project = ?''', (nowy_status, name, projekt))
+            elif len(zadanie_parts) == 2:
+                zadanie_nazwa = zadanie_parts[0]
+                self.cur.execute('''UPDATE tasks SET status = ? WHERE name = ? AND project IS NULL''', (nowy_status, name))
+            self.conn.commit()
+            self.wczytaj_zadania()
+
     def wczytaj_projekty(self):
+        self.projekt_combobox.clear()
         self.cur.execute('''SELECT DISTINCT project FROM tasks''')
         projekty = self.cur.fetchall()
         for projekt in projekty:
             self.projekt_combobox.addItem(projekt[0])
 
+    def start_poker_planning(self):
+        selected_item = self.lista_zadan.currentItem()
+        if selected_item is not None:
+            poker_points = self.poker_planning_edit.text()
+            if poker_points.strip():
+                selected_task = selected_item.text()
+                selected_task_parts = selected_task.split(" - ")
+                name = f"{selected_task_parts[0]} - {selected_task_parts[1]}"
+                projekt = selected_task_parts[1]
+                self.cur.execute('''UPDATE tasks SET poker_points = ? WHERE name = ? AND project = ?''',
+                                 (poker_points, name, projekt))
+                self.conn.commit()
+                QMessageBox.information(self, "Poker Planning",
+                                        f"Zadanie: {selected_task}\n\nPunkty Poker Planning: {poker_points}")
+                self.poker_planning_edit.clear()
+            else:
+                QMessageBox.warning(self, "Błąd", "Podaj punkty Poker Planning")
+        else:
+            QMessageBox.warning(self, "Błąd", "Wybierz zadanie")
+        self.wczytaj_zadania()
+
     def wczytaj_zadania(self):
+
         self.lista_zadan.clear()
         projekt = self.projekt_combobox.currentText()
-        self.cur.execute('''SELECT name FROM tasks WHERE project = ?''', (projekt,))
-        zadania = self.cur.fetchall()
-        for zadanie in zadania:
-            self.lista_zadan.addItem(zadanie[0])
+
+        statuses = ["Do zrobienia", "W trakcie", "Wykonane"]
+        for status in statuses:
+            self.cur.execute('''SELECT name, poker_points FROM tasks WHERE project = ? AND status = ?''', (projekt, status))
+            zadania = self.cur.fetchall()
+            if zadania:
+                column_header = QListWidgetItem(status)
+                column_header.setFlags(column_header.flags() & ~QtCore.Qt.ItemIsSelectable)
+                column_header.setBackground(QtGui.QColor("lightgray"))
+                self.lista_zadan.addItem(column_header)
+
+                for zadanie in zadania:
+                    item = QListWidgetItem(f"{zadanie[0]} - {zadanie[1]}")
+                    self.lista_zadan.addItem(item)
 
 
 if __name__ == "__main__":
